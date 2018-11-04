f0326600cbd47159388ba3e1b8c8bbf82b421bcb
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-11-04 14:33:48 -0500

    Scripting first microgame

---
+++ Assets/Scripts/Balloon_popping.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon_popping : MonoBehaviour {
+
+	public GameObject balloon_template;
+
+	// Use this for initialization
+	void Start() {
+		for (int i = 0; i < 10; i++) {
+			Instantiate(balloon_template, new Vector3(i * 2f, 1f, 0f), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
